8cef1d98acac132260341ed79d0afbfaaebe8493
Brianojee <brian@codeinstitute.net>
2018-07-05 05:48:48 +0000

    added and wired tenplates

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "It's Alive!!!"
+def index():
+    return render_template("index.html")
 
-
+@app.route('/about')
+def about():
+    return render_template("about.html")
+    
 if __name__ == '__main__' :
-    app.run()
+    app.run(host = os.environ.get("IP"),
+            port = os.environ.get('PORT'),
+            debug=True)
